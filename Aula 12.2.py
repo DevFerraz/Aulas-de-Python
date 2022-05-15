9d619b815e59fd4c81d901a4dc581151722c1ffd
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-15 19:56:11 -0300

    Update Aula 12.2.py

--- Aula 12.2.py
+++ Aula 12.2.py
@@ -1,10 +1,10 @@
 num = int(input('Escreva um numero inteiro: '))
 num2 = int(input('Escolha (1) para base binaria, (2) para octal e (3) para hexadecimal: '))
 if num2 == int('1'):
-    print(bin(num))
+    print('{} em binario eh: {}' .format(num, bin(num)[2:]))
 elif num2 == int('2'):
-    print(oct(num))
+    print('{} em octal eh: {}'.format(num, oct(num)[2:]))
 elif num2 == int('3'):
-    print(hex(num))
+    print('{} em hexadecimal eh: {}' .format(num, hex(num)[2:]))
 else:
     print('Voce digitou uma opcao invalida!')
