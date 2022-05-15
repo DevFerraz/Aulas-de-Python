8d2fbf5838c9946675332a74a4d50f480e7ca1c0
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-15 12:48:04 -0300

    Create Aula 12.7.py

---
+++ Aula 12.7.py
@@ -0,0 +1,16 @@
+import time
+n1 = float(input('Digite o valor da primeira reta para formar um triangulo: '))
+n2 = float(input('Digite o valor da segunda reta: '))
+n3 = float(input('Digite o valor da terceira reta: '))
+n4 = n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2
+if n4 == 1:
+    print('Otimo, esses sao valores possiveis para formar um triangulo! ')
+else:
+    print('Nao eh possivel formar nenhum triangulo! ')
+time.sleep(1)
+if n1 == n2 == n3 and n4 == 1:
+    print('O triangulo formado foi equilatero! ')
+elif n1 == n2 or n1 == n3 or n2 == n3 and n4 == 1:
+    print('O triangulo formado foi isosceles! ')
+elif n1 != n2 != n3 and n4 == 1:
+    print('O triangulo formado foi escaleno! ')
