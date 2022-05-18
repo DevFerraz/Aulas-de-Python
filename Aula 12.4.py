1b352da4b37eaabd273b0c75e50dd7a026a44f4c
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-17 21:18:37 -0300

    Update Aula 12.4.py

--- Aula 12.4.py
+++ Aula 12.4.py
@@ -1,10 +1,15 @@
 from datetime import datetime
-date = str(input('Digite sua data de nascimento (modelo dd-mm-aaaa): '))
-data = datetime.strptime(date)
-today = datetime.today()
-if date < today:
-    print('Ainda nao esta na hora de se alistar! ')
-elif date > today:
-    print('Ja passou da hora de se alistar! ')
-elif date == today:
+nascimento = int(input('Digite o ano do seu nascimento: '))
+hoje = datetime.today().year
+idade = hoje - nascimento
+if idade < 18:
+    diferenca1 = 18 - idade
+    print('Ainda nao esta na hora de se alistar, ainda faltam {} anos! ' .format(diferenca1))
+    print('Voce deve se alistar em {}! ' .format(hoje + diferenca1))
+elif idade > 18:
+    diferenca2 = idade - 18
+    print('Ja passaram {} anos da hora de se alistar! ' .format(diferenca2))
+    print('Voce deveria ter se alistado em {}! ' .format(hoje - diferenca2))
+elif idade == 18:
     print('Eh hora de se alistar! ')
+
