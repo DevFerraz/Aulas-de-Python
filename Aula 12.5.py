d82dfadc5db734c92d5aae8d4ae3688e3d66651e
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-17 21:45:23 -0300

    Update Aula 12.5.py

--- Aula 12.5.py
+++ Aula 12.5.py
@@ -1,9 +1,10 @@
 nota1 = float(input('Digite a primeira nota do aluno: '))
 nota2 = float(input('Digite a segunda nota do aluno: '))
 nota_final = (nota1 + nota2)/2
+print('A nota final do aluno foi: {:.1f}' .format(nota_final))
 if nota_final < 5:
     print('O aluno foi reprovado! ')
-elif nota_final <= 5:
+elif nota_final < 7:
     print('O aluno esta de recuperacao! ')
 else:
     print('O aluno foi aprovado! ')
