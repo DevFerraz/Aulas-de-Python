851349759ead5d5cd357f23b67884fb1fec3ac2f
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-24 13:20:50 -0300

    update

--- Aulas/Aula 13.2.py
+++ Aulas/Aula 13.2.py
@@ -1,7 +1,9 @@
 soma = 0
+cont = 0
 for contagem in range(3, 498, 3):
     if contagem % 2 != 0:
+        cont = cont + 1
         print('{} + {}' .format(soma, contagem))
         soma = soma + contagem
 
-print(soma)
+print('A soma dos numeros eh: {} e a quantidade dos numeros eh: {}' .format(soma, cont))
--- Aulas/Aula 13.4.py
+++ Aulas/Aula 13.4.py
@@ -1,6 +1,6 @@
 soma = 0
-for c in range(0, 6):
-    num = int(input('Digite um numero inteiro: '))
+for c in range(1, 7):
+    num = int(input('Digite {}º valor: ' .format(c)))
     if num % 2 == 0:
         soma += num
 print('A soma dos numeros pares eh igual a: {}'.format(soma))
