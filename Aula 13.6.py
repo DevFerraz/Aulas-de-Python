9699f584139c572b84ebe362f380ac7237db01d9
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-05-25 13:25:23 -0300

    Update Aula 13.6.py

--- Aula 13.6.py
+++ Aula 13.6.py
@@ -1,10 +1,13 @@
 num = int(input('Digite um numero inteiro qualquer maior do que 0: '))
-condicao = 1
-memoria = 0
-for c in range(1, num):
-    if num % condicao == 0:
-        memoria = condicao
-        if memoria > 1 or memoria < num:
-            print('Este numero nao eh primo! ')
-        else:
-            print('Este eh um numero primo! ')
+div = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m', end='')
+        div += 1
+    else:
+        print('\033[31m', end='')
+    print('{} ' .format(c), end='')
+if div == 2:
+    print('\n\33[mEste numero foi divisivel apenas {} vezes, logo, eh um numero primo! ' .format(div))
+else:
+    print('\n\33[mEste numero foi divisivel {} vezes, logo, nao eh um numero primo! ' .format(div))
