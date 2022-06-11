3ff509c663a1a3dbe28c8f7fffd211e77df23a4b
devferraz <natanferraz.pf@gmail.com>
2022-06-11 20:57:26 -0300

    Update Aula 13.6.py

--- Aula 12.3.py
+++ Aula 12.3.py
@@ -1,8 +1,5 @@
-try:
-    num1 = str(input('Digite um valor qualquer: '))
-    num2 = str(input('Digite outro valor qualquer: '))
-print('Voce tem certeza que digitou valores validos? ')
-except:
+num1 = float(input('Digite um valor qualquer: '))
+num2 = float(input('Digite outro valor qualquer: '))
 if num1 > num2:
     print('O primeiro valor digitado eh maior que o segundo! ')
 elif num1 < num2:
