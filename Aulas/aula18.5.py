ac15b7f9379a91460d286434bb61054359dd3aaa
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-07-12 19:10:26 -0300

    commit

--- Aulas/aula18.5.py
+++ Aulas/aula18.5.py
@@ -1,28 +1,31 @@
 completa = []
-dados = []
-medias = []
 while True:
-    dados.append(str(input('Digite o nome do aluno: ')))
+    nome = (str(input('Digite o nome do aluno: ')))
     n1 = float(input('Digite a primeira nota do aluno: '))
     n2 = float(input('Digite a segunda nota do aluno: '))
     m = (n1 + n2) / 2
-    dados.append(n1)
-    dados.append(n2)
-    medias.append(m)
-    completa.append(dados[:])
-    dados.clear()
+    completa.append([nome, [n1, n2], m])
+    if m < 0:
+        print('Notas inválidas! ')
+        completa.pop()
+    elif m > 10:
+        print('Notas inválidas! ')
+        completa.pop()
     r = str(input('Deseja cadastrar outro aluno? [S/N] '))
     if r in 'Ss':
-        completa == completa
+        completa = completa
     elif r in 'Nn':
         break
     else:
         print('Resposta inválida! ')
-for c, v in enumerate(medias):
-    print(f'A média do {c+1} aluno é {v}')
-    if 6 < v <= 10:
-        print('O aluno foi aprovado.')
-    elif 0 < v < 6:
-        print('O aluno está reprovado. ')
+for c, v in enumerate(completa):
+    print(f'{c+1:<4} A média do aluno {v[0]:<6} é {v[2]:>5.1f}')
+while True:
+    opc = int(input('Mostrar notas de qual aluno? (Digite 999 para sair): '))
+    if opc == 999:
+        break
+    if opc <= len(completa):
+        print(f'As notas de {completa[opc - 1][0]}, são: {completa[opc - 1][1]}')
     else:
-        print('A nota da média é inválida. ')
+        print('Opção inválida! ')
+print('-=' * 7, 'Muito Obrigado! Até Logo!', '-=' * 7)
