bfc91c0d8ddc36f10a33e113184002eec95de54f
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-08-01 20:38:52 -0300

    commit

--- Aulas/aula21.3.py
+++ Aulas/aula21.3.py
@@ -1,30 +1,32 @@
-def fatorial(n=0, show='n'):
+def fatorial(n=0, show=False):
     f = 1
-    c1 = 0
     print('Os números gerados na progressão foram:')
     for c in range(n, 0, -1):
         f *= c
-        c1 += 1
-        if show in 'Nn':
-            return f
-        elif show in 'Ss':
-            print(f'{f}')
-        else:
-            return 0
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+                print(f)
+                print()
     return f
 
 
-while True:
+def fatorial1():
     fator1 = int(input('Digite o número que deseja saber o fatorial: '))
-    fator2 = str(input('Deseja saber os números gerados na progressão? '))
+    fator2 = bool(input('Deseja saber os números gerados na progressão? '))
     fatorial(fator1, fator2)
-    while True:
-        r = str(input('Deseja continuar? [S/N] '))
-        if r in 'Nn':
-            break
-        elif r in 'Ss':
-            r = r
-        else:
-            print('Resposta inválida! Tente novamente.')
+
+
+fatorial1()
+while True:
+    r = str(input('Deseja continuar? [S/N] '))
     if r in 'Nn':
         break
+    elif r in 'Ss':
+        fatorial1()
+    else:
+        print('Resposta inválida! Tente novamente.')
+
