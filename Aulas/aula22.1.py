b049dd6006df31a853fe6bb7b2b906d6d5712895
Natan Cintra Ferraz <104537447+DevFerraz@users.noreply.github.com>
2022-08-09 19:36:10 -0300

    commit

--- Aulas/aula22.1.py
+++ Aulas/aula22.1.py
@@ -1,10 +1,53 @@
 from uteis import moeda
 
 
-valor = float(input('Digite o valor da moeda que deseja: '))
-print(f'O dobro do valor é: {moeda.dobro_moeda(valor)}')
-print(f'A metade do valor é: {moeda.metade_moeda(valor)}')
-porcentagem = int(input('Digite uma porcentagem que deseja aumentar no valor inserido: '))
-print(f'O valor {valor} com {porcentagem}% de acréscimo fica em {moeda.aumentar_moeda(valor, porcentagem)}')
-porcentagem1 = int(input('Digite uma porcentagem que deseja diminuir do valor inserido: '))
-print(f'O valor {valor} com {porcentagem1}% de diminuição fica em {moeda.diminuir_moeda(valor, porcentagem)}')
+def programa():
+    while True:
+        valor = str(input('Digite o valor da moeda que deseja: R$'))
+        if valor.isalpha():
+            valor = -1
+        valor = float(valor)
+        if valor == -1:
+            print(moeda.dobro_moeda(valor))
+            break
+        print(f'O dobro do valor é: R${moeda.dobro_moeda(valor):.2f}')
+        print(f'A metade do valor é: R${moeda.metade_moeda(valor):.2f}')
+        while True:
+            porcentagem = str(input('Digite uma porcentagem que deseja aumentar no valor inserido: '))
+            if porcentagem.isnumeric():
+                porcentagem = int(porcentagem)
+                break
+            else:
+                print('Valor inválido!')
+        print(f'O valor R${valor} com {porcentagem}% de acréscimo fica em R${moeda.aumentar_moeda(valor, porcentagem):.2f}')
+        while True:
+            porcentagem1 = str(input('Digite uma porcentagem que deseja diminuir do valor inserido: '))
+            if porcentagem1.isnumeric():
+                porcentagem1 = int(porcentagem1)
+                break
+            else:
+                print('Valor inválido!')
+        print(f'O valor R${valor} com {porcentagem1}% de diminuição fica em R${moeda.diminuir_moeda(valor, porcentagem):.2f}')
+        while True:
+            r1 = str(input('Deseja continuar? [S/N] '))
+            if r1 in 'Ss':
+                print('OK!')
+                break
+            elif r1 in 'Nn':
+                break
+            else:
+                print('Resposta inválida!')
+        if r1 in 'Nn':
+            break
+
+
+programa()
+while True:
+    r = str(input('Quer tentar novamente? [S/N] '))
+    if r in 'Nn':
+        print('Obrigado! Volte Sempre!')
+        break
+    elif r in 'Ss':
+        programa()
+    else:
+        print('Resposta inválida')
--- Aulas/uteis/moeda/__init__.py
+++ Aulas/uteis/moeda/__init__.py
@@ -1,8 +1,22 @@
 def dobro_moeda(n):
+    if n == -1:
+        return 'Número inválido!'
     return n * 2
+
+
 def metade_moeda(n):
+    if n == -1:
+        return 'Número inválido!'
     return n / 2
+
+
 def aumentar_moeda(n, p):
-   return n + n * p / 100
+    if n == -1:
+        return 'Número inválido!'
+    return n + n * p / 100
+
+
 def diminuir_moeda(n, p):
-   return n - n * p / 100
+    if n == -1:
+        return 'Número inválido!'
+    return n - n * p / 100
